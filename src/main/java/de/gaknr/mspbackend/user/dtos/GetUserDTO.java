78b1662ee989cc83488fe97f2d3a01a9fc4ef4ca
Zanatros <mineplayslpemail@gmail.com>
2024-03-20 17:40:15 +0100

    MSP-14 Added GetUserDTO.java to user domain

---
+++ src/main/java/de/gaknr/mspbackend/user/dtos/GetUserDTO.java
@@ -0,0 +1,17 @@
+package de.gaknr.mspbackend.user.dtos;
+
+import de.gaknr.mspbackend.clothingitem.dtos.GetClothingItemDTO;
+
+import de.gaknr.mspbackend.outfit.dtos.GetOutfitDTO;
+
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+public record GetUserDTO(
+    ObjectId id,
+    String keycloakUserId,
+    List<GetClothingItemDTO> closet,
+    List<GetOutfitDTO> outfits
+) {
+}
