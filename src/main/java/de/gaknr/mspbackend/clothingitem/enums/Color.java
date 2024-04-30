11d33232e4d023d0713caf1c5dbc08e3a227751e
Niels Hoppe <niels.hoppe@governikus.de>
2024-04-30 14:59:43 +0200

    MSP-45 add enum functions to group colors

--- src/main/java/de/gaknr/mspbackend/clothingitem/enums/Color.java
+++ src/main/java/de/gaknr/mspbackend/clothingitem/enums/Color.java
@@ -48,6 +48,77 @@ public enum Color {
     White,
     Green,
     Brown,
-    Taupe
+    Taupe;
+
+    public Boolean isBlue(Color color) {
+        return color == Color.Navy_Blue ||
+            color == Color.Turquoise_Blue ||
+            color == Color.Blue;
+    }
+
+    public Boolean isRed(Color color) {
+        return color == Color.Maroon ||
+            color == Color.Red ||
+            color == Color.Burgundy;
+    }
+
+    public Boolean isGreen(Color color) {
+        return color == Color.Sea_Green ||
+            color == Color.Olive ||
+            color == Color.Khaki ||
+            color == Color.Teal ||
+            color == Color.Lime_Green ||
+            color == Color.Green;
+    }
+
+    public Boolean isYellow(Color color) {
+        return color == Color.Yellow ||
+            color == Color.Gold;
+    }
+
+    public Boolean isPink(Color color) {
+        return color == Color.Pink ||
+            color == Color.Purple ||
+            color == Color.Magenta ||
+            color == Color.Mauve ||
+            color == Color.Nude ||
+            color == Color.Rose ||
+            color == Color.Lavender;
+    }
+
+    public Boolean isOrange(Color color) {
+        return color == Color.Peach ||
+            color == Color.Orange ||
+            color == Color.Rust;
+    }
+
+    public Boolean isBrown(Color color) {
+        return color == Color.Copper ||
+            color == Color.Bronze ||
+            color == Color.Beige ||
+            color == Color.Coffee_Brown ||
+            color == Color.Tan ||
+            color == Color.Cream ||
+            color == Color.Mushroom_Brown ||
+            color == Color.Brown ||
+            color == Color.Taupe;
+    }
+
+    public Boolean isBlack(Color color) {
+        return color == Color.Black ||
+            color == Color.Charcoal ||
+            color == Color.NA ||
+            color == Color.Multi;
+    }
+
+    public Boolean isWhite(Color color) {
+        return color == Color.Steel ||
+            color == Color.Metallic ||
+            color == Color.Silver ||
+            color == Color.Grey_Melange ||
+            color == Color.Grey ||
+            color == Color.Off_White ||
+            color == Color.White;
+    }
 
 }
